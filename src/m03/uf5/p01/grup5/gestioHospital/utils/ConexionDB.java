6bdb4a693f236ba30e1e92dd18c48a97ae3ff5c4
roger <roger@192.168.1.163>
2019-03-30 15:27:27 +0100

    Merge origin/GraphicVersion into GraphicVersion
    Conflicts:
    	proyecto_uf5p01_gh/saved/pacients.csv

---
+++ src/m03/uf5/p01/grup5/gestioHospital/utils/ConexionDB.java
@@ -0,0 +1,29 @@
+package m03.uf5.p01.grup5.gestioHospital.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConexionDB {
+    public static String url = "jdbc:mysql://127.0.0.1:3306/bddeprueba",
+            usuario = "root",
+            pass = "maduro",
+            clase = "com.mysql.jdbc.Driver";
+    
+    public static Connection contectar(){
+        Connection conexion = null;
+        try{
+            Class.forName(clase);
+            conexion = (Connection) DriverManager.getConnection(url,usuario,pass);
+            System.out.println("Conexión establecida");
+        }catch (Exception e){
+            System.out.println("Error de conexión a la BD en ConexionDB.java");
+            System.out.println(e.getMessage());
+        }
+        return conexion;
+    }
+}
